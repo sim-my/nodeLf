b9994e45ad9ab6de13a33ec0920ecbe9f6bd3183
Simran Panthi <simranpanthi101@gmail.com>
2021-02-18 10:21:15 +0545

    No api middleware added

--- index.js
+++ index.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const route = require('./route.js');
 
+
+
 app.use('/api', route);
+
+app.use((req, res) => {
+    res.json({
+        msg: 'API not found.'
+    })
+});
 
 
 app.listen(3000);
